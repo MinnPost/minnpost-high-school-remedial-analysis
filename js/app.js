a5c664f63fb69d9b36f837134910a7ab597a9751
Alan Palazzolo <apalazzolo@minnpost.com>
2014-12-16 10:38:18 -0600

    Some basic sstructureing.

--- js/app.js
+++ js/app.js
@@ -32,55 +32,57 @@ require([
     initialize: function() {
       var thisApp = this;
 
+      // TODO: Use a router so people can link to specific schools
+
       // Create main application view
       this.mainView = new Ractive({
         el: this.$el,
         template: tApplication,
         data: {
+          dataLoaded: true
         },
         partials: {
+        }
+      });
+
+      // Render when data is loaded
+      this.mainView.observe('dataLoaded', function(n, o) {
+        if (!_.isUndefined(n) && n === true) {
+          thisApp.drawCharts();
+          thisApp.drawMaps();
         }
       });
 
       // Get data
     },
 
-
-    // Make some example depending on what parts were asked for in the
-    // templating process.  Remove, rename, or alter this.
-    makeExamples: function() {
+    // Draw charts
+    drawCharts: function() {
 
       var exampleData = [{
         name: 'Example',
-        data: [ 6 , 11, 32, 110, 235, 369, 640, 1005, 1436, 2063, 3057, 4618, 6444, 9822, 15468, 20434, 24126, 27387, 29459, 31056, 31982, 32040, 31233, 29224, 27342, 26662, 26956, 27912, 28999, 28965, 27826, 25579, 25722, 24826, 24605, 24304, 23464, 23708, 24099, 24357, 24237, 24401, 24344, 23586, 22380, 21004, 17287, 14747, 13076, 12555, 12144, 11009, 10950, 10871, 10824, 10577, 10527, 10475, 10421, 10358, 10295, 10104 ]
+        data: [
+          ['School 1', 4],
+          ['School 2', 5],
+          ['School 3', 6],
+          ['School 4', 7],
+          ['School 5', 8]
+        ]
       }];
 
-      // Line chart
-      mpHighcharts.makeChart(this.$('.chart-line-example'),
-        $.extend(true, {}, mpHighcharts.lineOptions, {
-          colors: _.sample(_.values(mpConfig['colors-data']), 3),
-          series: exampleData,
-          legend: { enabled: false },
-          yAxis: {
-            title: { enabled: false }
-          }
-        }
-      ));
-
       // Column chart
-      mpHighcharts.makeChart(this.$('.chart-column-example'),
+      mpHighcharts.makeChart(this.$('.schools-chart'),
         $.extend(true, {}, mpHighcharts.columnOptions, {
-          colors: _.sample(_.values(mpConfig['colors-data']), 3),
+          colors: mpConfig['colors-data'][0],
           series: exampleData,
           legend: { enabled: false }
         }
       ));
+    },
 
-
-
-
-
-      var markerMap = mpMaps.makeLeafletMap('example-markers-features-map');
+    // Draw map
+    drawMaps: function() {
+      var markerMap = mpMaps.makeLeafletMap('schools-map');
       var tooltipControl = new mpMaps.TooltipControl();
       markerMap.setZoom(9);
       markerMap.addControl(tooltipControl);
